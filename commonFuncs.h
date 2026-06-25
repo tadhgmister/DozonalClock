c9b53af59312c837496bdc6c756fae6253d1d57f
Tadhg McDonald-Jensen <tadhgmister@gmail.com>
2026-06-24 20:43:04 -0400

    update commonFunc.h to match prev commit

--- commonFuncs.h
+++ commonFuncs.h
@@ -1,6 +1,9 @@
 #ifndef COMMON_FUNCS_H
 #define COMMON_FUNCS_H
 char read_selector_dial();
-void write_digit(byte idx, char n);
+void erase_digit(byte dig);
+void write_glyph_raw(byte idx, char n);
 void write_number(unsigned long n);
+void write_digit_direct(byte dig, char value);
+void update_digit(byte digit, unsigned long n);
 #endif
